56f97fe5c02f1b7f06c40f5c8dd6eab82856a052
Hilton Tavares <hilton.edeir@gmail.com>
2024-04-07 20:55:55 +0100

    update

--- Show calender to a given month and year.py
+++ Show calender to a given month and year.py
@@ -1,7 +1,7 @@
 import calendar
 
-
-data = input("Insira uma data no formato MM/AAAA: ")
+data = input("\nInsira uma data no formato MM/AAAA: ")
 lista_data = data.split("/")
 
+print("")
 print(calendar.month(int(lista_data[1]), int(lista_data[0])))
